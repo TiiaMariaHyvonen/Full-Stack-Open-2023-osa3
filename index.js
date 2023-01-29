1bb66ff7390f048964350fde567481d14206727d
TiiaMariaHyvonen <tiiamariahyvonen@outlook.com>
2023-01-29 13:38:07 +0200

    3.5-3.6

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
-        id: 1,
         name: 'Arto Hellas', 
-        number: '040-123456' 
+        number: '040-123456',
+        id: 1 
     },
     { 
-        id: 2,
         name: 'Ada Lovelace', 
-        number: '39-44-5323523' 
-    },
-    {   id: 3,
-        name: 'Dan Abramov', 
-        number: '12-43-234345' 
+        number: '39-44-5323523',
+        id: 2 
     },
     {   
-        id: 4, 
+        name: 'Dan Abramov', 
+        number: '12-43-234345',
+        id: 3 
+    },
+    {   
         name: 'Mary Poppendieck', 
-        number: '39-23-6423122' 
+        number: '39-23-6423122',
+        id: 4 
     }
 ]
 
@@ -43,13 +46,37 @@ let persons = [
   })
 
   app.delete('/api/persons/:id', (request, response) => {
-    console.log(persons)
-    console.log("---------")
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
-  console.log(persons)
     response.status(204).end()
   })
+
+  const generateId = () => {
+    const maxId = Math.floor(Math.random() * 5000)
+    return maxId + 1
+  }
+
+  app.post('/api/persons', (request, response) => {
+    const person = request.body
+    person.id = generateId()
+    if (!person.name || !person.number) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+    const existing_person = persons.find(p => p.name === person.name)
+    if (existing_person) {
+      return response.status(400).json({ 
+        error: 'name must be unique' 
+      })
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+  })
+
+
+
   
   const PORT = 3001
   app.listen(PORT, () => {
