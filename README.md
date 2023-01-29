70f9370d54b05f211bda27b3267cca80bac24dc8
TiiaMariaHyvonen <tiiamariahyvonen@outlook.com>
2023-01-29 21:14:19 +0200

    readme added

---
+++ README.md
@@ -0,0 +1,1 @@
+Linkki: https://osa3.onrender.com
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 var morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
+app.use(cors())
 app.use(express.json())
-
 morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 
 let persons = [
     { 
@@ -81,8 +84,7 @@ let persons = [
 
 
 
-  
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
