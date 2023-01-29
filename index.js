6f0a08457768ba22237dcb6eb4121f047533e247
TiiaMariaHyvonen <tiiamariahyvonen@outlook.com>
2023-01-29 21:50:28 +0200

    frontend tuotantoversio lisätty

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 morgan.token('body', (req, res) => { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
+app.use(express.static('build'))
 
 let persons = [
     { 
