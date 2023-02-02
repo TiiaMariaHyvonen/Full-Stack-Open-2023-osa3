d71de6ad7e5ecd08fed2461419fde9584e9e083f
TiiaMariaHyvonen <tiiamariahyvonen@outlook.com>
2023-02-02 18:13:35 +0200

    3.17*: puhelinluettelo ja tietokanta, step5

--- index.js
+++ index.js
@@ -48,7 +48,20 @@ app.use(express.static('build'))
     .catch(error => next(error))
   })
 
-  app.post('/api/persons', (request, response) => { //doesn't take existing names into account yet
+  app.put('/api/persons/:id', (request, response, next) => { 
+    const body = request.body
+    const person = {
+      name: body.name,
+      number: body.number
+    }
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+  })
+
+  app.post('/api/persons', (request, response) => {
     const body = request.body
     if (!body.name || !body.number) {
       return response.status(400).json({ error: 'content missing' })
